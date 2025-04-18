84f4fc254f44783a8cb32cffa98717aa21c37853
Greeicy_vgb <110705217+Vgreeicy09@users.noreply.github.com>
2025-04-18 17:02:26 +0000

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ArticleController;
 
+
+Route::get('/', function () {
+    return view('welcome');
+});
 // Route de la page d'accueil (affiche les derniers articles)
 Route::get('/', [ArticleController::class, 'index'])->name('home');
 
